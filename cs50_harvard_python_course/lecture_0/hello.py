43a81a157a0130ebff9d8d962cb663613127f73e
Dagim Sime <simedagim121@gmail.com>
2026-02-02 05:03:54 +0300

    cs50: improving first python program

--- cs50_harvard_python_course/lecture_0/hello.py
+++ cs50_harvard_python_course/lecture_0/hello.py
@@ -6,3 +6,23 @@ input("What's your name? ")
 print("hello, world")
 # This edit alone, however, will not allow your program to output what your user inputs. For that, 
 # we will need to introduce you to variables
+
+
+
+# Further Improving Your First Python Program
+# We can further edit our code as follows:
+
+#  Ask the user for their name
+name = input("What's your name? ")
+
+#  Print hello and the inputted name
+print("hello, " + name)
+# It turns out that some functions take many arguments.
+# We can use a comma , to pass in multiple arguments by editing our code as follows:
+
+#  Ask the user for their name
+name = input("What's your name? ")
+
+# Print hello and the inputted name
+print("hello,", name)
+# The output in the terminal, if we typed “Dagim” we would be hello, Dagim. Success.
