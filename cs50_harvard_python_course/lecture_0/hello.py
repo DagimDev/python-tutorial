d2d63228b2f285be23835c7c7ddcbbb82ff1dfe0
Dagim Sime <simedagim121@gmail.com>
2026-02-02 05:01:12 +0300

    cs50: First Python Program

---
+++ cs50_harvard_python_course/lecture_0/hello.py
@@ -0,0 +1,8 @@
+# Improving Your First Python Program
+# In our text editor in hello.py we can add another function. input is a function that takes a prompt 
+# as an argument. We can edit our code to say
+
+input("What's your name? ")
+print("hello, world")
+# This edit alone, however, will not allow your program to output what your user inputs. For that, 
+# we will need to introduce you to variables
